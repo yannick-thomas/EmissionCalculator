246e782b22b382fcdf81c060b32f085587801955
Yannick Thomas <hello@yannick-thomas.de>
2023-02-15 12:34:07 +0100

    added print functionality

---
+++ print_handler.py
@@ -0,0 +1,26 @@
+from xhtml2pdf import pisa
+import subprocess
+from jinja2 import Environment, FileSystemLoader
+
+class PrintHandler():
+    
+    def __init__(self, results : list) -> None:
+        self.results = results
+
+    def build_template(self): 
+        environment = Environment(loader=FileSystemLoader("."))
+        template = environment.get_template("print_template.html")
+
+        return template.render(
+            emission_component_result = self.results[2],
+            emissions = self.results[1],
+            energy_content = self.results[3]
+        )
+    
+    def convert_template(self):
+        with open("label.pdf", "w+b") as output_file:
+            pisa.CreatePDF(self.build_template(), dest=output_file, pagesize=(210, 298))
+
+    def print_result(self):
+        self.convert_template()
+        subprocess.call(['rundll32','shimgvw.dll,ImageView_PrintTo', 'label.pdf'])
