78701ac9b210da13fb88b0abd6a49d12c2ed378e
aehliglucas <lucas@aehlig.com>
2023-02-24 19:17:17 +0100

    adjusted handler to work after being compiled

--- calculator/misc/print_handler.py
+++ calculator/misc/print_handler.py
@@ -1,17 +1,43 @@
 from xhtml2pdf import pisa
+from reportlab.graphics.barcode import code128
+from reportlab.graphics.barcode import code93
+from reportlab.graphics.barcode import code39
+from reportlab.graphics.barcode import usps
+from reportlab.graphics.barcode import usps4s
+from reportlab.graphics.barcode import ecc200datamatrix
 import os
-from jinja2 import Environment, FileSystemLoader
+from jinja2 import Environment, BaseLoader
 import re
-
+import tempfile
 
 class PrintHandler():
 
     def __init__(self, results : list) -> None:
+        self.tempfile_path = os.path.join(os.getcwd(), "label.pdf")
         self.results = results
 
     def build_template(self): 
-        environment = Environment(loader=FileSystemLoader("templates/"))
-        template = environment.get_template("print_template.html")
+        template = Environment(loader=BaseLoader).from_string("""
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <style>
+                    @page {
+                        size: 2481px 2398px landscape;
+                        margin-left: 239.8;
+                        margin-top: 744.3;
+                    }
+                    p {
+                        font-size: 30px;
+                    }
+                </style>
+            </head>
+            <body>
+                <p><b>enth. CO2 Abgabe {{ emission_component_result }} EUR, Brutto</b> / CO2 Abgabe pro t 30,00 EUR, Netto</p>
+                <p>CO2 Kg der Lieferung {{ emissions }} / CO2 Kg pro kWh 2,2664 / kWh der Lieferung {{ energy_content }}</p>
+            </body>
+            </html>
+        """)
 
         return template.render(
             emission_component_result = str(self.results[2]).replace(".", ","),
@@ -20,9 +46,9 @@ class PrintHandler():
         )
 
     def convert_template(self):
-        with open("label.pdf", "w+b") as output_file:
+        with open(self.tempfile_path, "w+b") as output_file:
             pisa.CreatePDF(self.build_template(), dest=output_file, pagesize=(210, 203))
 
     def print_result(self):
         self.convert_template()
-        os.startfile("label.pdf")
+        os.startfile(self.tempfile_path)
